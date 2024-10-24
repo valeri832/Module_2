07d0c9f04ee066e2dd1a6c6a32580bb5c6e3111c
valeri832 <wpgnik@gmail.com>
2024-10-24 13:29:56 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,22 @@
+calls = 0
+def count_calls ():
+    global calls
+    calls += 1
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+def is_contains (string, list_to_search):
+    count_calls()
+    lower_string = string.lower()
+    return lower_string in (item.lower() for item in list_to_search)
+print(string_info('Capybara'))
+print(string_info('Hello'))
+print(string_info('Armageddon'))
+print(string_info('Go ahead'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBan
+print(is_contains('cycle', ['recycle', 'cyclic']))  # No matches
+print(calls)
+
+
+
+
